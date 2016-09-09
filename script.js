7b16652474872ba720d390466d41cfece2c04967
aellis928 <mrskawasaki.90@gmail.com>
2016-09-09 15:28:07 -0700

    finished animation of bbd

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ $(document).ready(function(){
 	console.log('made it to the script.js!')
 })
 
+$(function(){
+	setTimeout(function() {
+		$('.fly-in-text').removeClass('hidden');
+	}, 500)
+	setTimeout(function() {
+		$('.fly-in-text2').removeClass('hidden');
+	}, 500)
+	setTimeout(function() {
+		$('.fly-in-text3').removeClass('hidden');
+	}, 500)
+
+})()
